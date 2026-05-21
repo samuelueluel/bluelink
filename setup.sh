81ad6fe7006d0cf1762f4941aca550aa3292ad11
Samuel <samuel@example.com>
2026-05-21 11:52:31 -0600

    Add ffmpeg-full codec runtime

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ echo "=== Adding Flathub Beta ==="
 flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 
 echo ""
+echo "=== Installing codecs ==="
+flatpak install -y flathub org.freedesktop.Platform.ffmpeg-full
+
+echo ""
 echo "=== Installing Stremio (beta) ==="
 flatpak install -y flathub-beta com.stremio.Stremio
 
